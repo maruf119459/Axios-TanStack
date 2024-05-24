5fdfbcc1fd7bf8f35f39cea87aea3e57aca6c03a
Md.Mohiuddin Maruf <116703654+maruf119459@users.noreply.github.com>
2024-05-24 21:13:05 +0600

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,57 @@
+/* main.jsx file*/
+
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query'
+import AuthProviders from './../AuthProviders.jsx';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <AuthProviders>
+        <RouterProvider router={router} />
+      </AuthProviders>
+    </QueryClientProvider>
+  </React.StrictMode>,
+)
+
+
+/*Example.jsx file*/
+function Example() {
+  const { isPending, error, data } = useQuery({
+    queryKey: ['repoData'], 
+    queryFn: () =>
+      fetch('https://api.github.com/repos/TanStack/query').then((res) =>
+        res.json(),
+      ),
+  })
+
+  if (isPending) return 'Loading...'
+
+  if (error) return 'An error has occurred: ' + error.message
+
+  return (
+    <div>
+      <h1>{data.name}</h1>
+      <p>{data.description}</p>
+      <strong>👀 {data.subscribers_count}</strong>{' '}
+      <strong>✨ {data.stargazers_count}</strong>{' '}
+      <strong>🍴 {data.forks_count}</strong>
+    </div>
+  )
+}
